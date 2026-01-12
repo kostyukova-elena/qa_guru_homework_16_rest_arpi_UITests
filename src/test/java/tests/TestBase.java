df1eb19c7de82da500287052bc1f041512a75e4c
ekostyukova <kostyukova_ea@mail.ru>
2026-01-12 14:25:45 +0500

    fix2

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.restassured.RestAssured;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
@@ -11,11 +12,12 @@ public class TestBase {
 
     @BeforeAll
     public static void setup() {
+        WebDriverManager.chromedriver().setup();
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.headless = true;
         Configuration.browser = "chrome";
-        Configuration.timeout = 4000;
+        Configuration.timeout = 10000;
         Configuration.baseUrl = "https://demoqa.com";
         RestAssured.baseURI = "https://demoqa.com";
     }
